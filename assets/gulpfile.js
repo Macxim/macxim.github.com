7573febe37d487dfcfc5110735023cd95331a39c
Maxime Laforet <maxime10081985@gmail.com>
2015-02-25 00:25:04 +0100

    Update gulpfile

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -2,21 +2,29 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     cssnext = require("gulp-cssnext");
 
-gulp.task('default', function() {
-  gulp.src("css/stylesheets/index.css")
-    .pipe(cssnext({
-      compress: false,
-      features: {
-        import: {
-          path: [
-            "node_modules",
-          ]
-        },
-        autoprefixer: {
-          browsers: ["last 2 versions"]
-        }
+gulp.task('styles', function() {
+  gulp.src("css/index.css")
+  .pipe(cssnext({
+    compress: false,
+    features: {
+      import: {
+        path: [
+          "node_modules"
+        ]
       }
-    }))
-    .pipe(gulp.dest("./dist/stylesheets"))
-  watch('css/**/*.css', ['./dist/stylesheets/'])
+    }
+  }))
+  .pipe(gulp.dest("./dist/"))
 });
+
+
+// build
+gulp.task("dist", [
+  "styles",
+])
+
+gulp.task("watch", ["dist"], function() {
+  gulp.watch("css/**/*.css", ["styles"])
+})
+
+gulp.task("default", ["dist", "watch"])
